cb71ea457bb1ea2434cb5401559ce058fee9c2b8
Kiiroyo <debo.nash1997@gmail.com>
2023-01-23 12:54:14 +0100

    premier map tsté sur hdfs

--- src/main/java/DataCleaning.java
+++ src/main/java/DataCleaning.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -68,16 +68,16 @@ public class DataCleaning extends Configured implements Tool {
 		job.setJarByClass(DataCleaning.class);
 		
 		MultipleInputs.addInputPath(job, new Path(args[0] + "P1/P1.csv" ), TextInputFormat.class, DataCleaningMapperP1.class);
-		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, JoinMapperRegions.class);
+		//MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, JoinMapperRegions.class);
 		
-		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputKeyClass(NullWritable.class);
 		job.setMapOutputValueClass(Text.class);
 
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
 		job.setOutputFormatClass(SequenceFileOutputFormat.class);
 		
-		TextOutputFormat.setOutputPath(job, new Path(args[2]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
--- src/main/java/Mappers/DataCleaningMapperP1.java
+++ src/main/java/Mappers/DataCleaningMapperP1.java
@@ -3,25 +3,35 @@ package Mappers;
 import java.io.IOException;
 
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class DataCleaningMapperP1 extends Mapper<LongWritable, Text, Text, Text>{
+
+//ID,Catégorie,horodate,vers CROUS,vers Cours de la Libération
+
+public class DataCleaningMapperP1 extends Mapper<LongWritable, Text, NullWritable, Text>{
 		
 		@Override
-		public void map(LongWritable key, Text value, Context context
-				) throws IOException, InterruptedException {
+		public void map(LongWritable key, Text value, Context context ) throws IOException, InterruptedException {
 			// on retire la premiere ligne (le header)
 			if(key.get() == 0L) return;
+
+			String[] data = value.toString().split(",");
 			
-			String[] tokens = value.toString().split(",");
-			String country = tokens[0].toLowerCase();
-			String regCode = tokens[3];
-			String cityName = tokens[1];
+			String[] hordatage = data[2].split(" ");
+			String[] time = hordatage[1].split(":");
 			
-			String k = country+","+regCode;
+			boolean in = data[3].equals("1");
 			
-			context.write(new Text(k), new Text(cityName + true));
+			String destination;
+			if (in){
+				destination = "Crous";
+			}else {
+				destination = "Cours de la Libération";
+			}
+			
+			context.write(NullWritable.get(), new Text(hordatage[0] + "," + time[0] + "," + time[1] + "," + in + "," + destination));
 		}
 }
 
