cbaad67fe369f136aee182d9d796036c051d6eea
achraf2000 <achrafhabachi100@gmail.com>
2023-01-23 22:17:54 +0100

    Adding DataCleaningMapperP10

---
+++ src/main/java/Mappers/DataCleaningMapperP10.java
@@ -0,0 +1,34 @@
+package Mappers;
+
+import Tools.NormalizedDate;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class DataCleaningMapperP10 extends Mapper<LongWritable, Text, Text, Text> {
+
+    // ID - Category - Horodate - vers sortie - vers entrée
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        if (key.get() == 0L) return;
+
+        String[] data = value.toString().split(",");
+
+        NormalizedDate date = new NormalizedDate(data[2]);
+
+        boolean out = data[3].equals("x");
+
+        String destination;
+        if (out) {
+            destination = "vers sortie";
+        } else {
+            destination = "vers entrée";
+        }
+
+        context.write(new Text("P10"), new Text(date.getDate() + "," + date.getHours() + "," + date.getMinutes() + "," + data[1] + "," + out + "," + destination));
+
+    }
+}
