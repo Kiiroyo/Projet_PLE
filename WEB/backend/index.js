b1f509c2f4be2faa9371212bbacf4d2072f5afcd
achraf2000 <achrafhabachi100@gmail.com>
2023-02-01 17:35:54 +0100

    first commit

---
+++ WEB/backend/index.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express')
+const app = express()
+const port = 3000
+
+const routes = require('./routes');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
